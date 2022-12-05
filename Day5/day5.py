61e271b3e5cd9785c6b5a644cdf84272fa750e83
DaraghD <daraghdownes12@gmail.com>
2022-12-05 10:54:17 +0000

    day5

---
+++ Day5/day5.py
@@ -0,0 +1,33 @@
+import re
+
+crate_dict = {1: ['D','T','W','F','J','S','H','N'],
+              2: ['H','R','P','Q','T','N','B','G'],
+              3: ['L','Q','V'],
+              4: ['N','B','S','W','R','Q'],
+              5: ['N','D','F','T','V','M','B'],
+              6: ['M','D','B','V','H','T','R'],
+              7: ['D','B','Q','J'],
+              8: ['D','N','J','V','R','Z','H','Q'],
+              9: ['B','N','H','M','S']}
+
+with open("input.txt") as file:
+    for line in file:
+        line = line.rstrip('\n')
+        line = re.findall(r"(\d+)", line)
+        line = [int(x) for x in line]
+        steps = line[0]
+        crate1 = line[1] #from
+        crate2 = line[2] #to
+        temp_crate = []
+
+        for i in range(steps):
+            x = crate_dict[crate1].pop()
+            temp_crate.append(x)
+            #crate_dict[crate2].append(x)
+        temp_crate.reverse()
+        for crate in temp_crate:
+            crate_dict[crate2].append(crate)
+
+
+for i in range(1,10):
+    print(crate_dict[i][-1])
