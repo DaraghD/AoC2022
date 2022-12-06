a091e2b0b4d405bcc8b8aaddf09a38d9822f379d
DaraghD <daraghdownes12@gmail.com>
2022-12-06 10:34:16 +0000

    day6

---
+++ Day6/day6.py
@@ -0,0 +1,13 @@
+with open("input.txt") as file:
+    for line in file:
+        temp = []
+        line = line.rstrip("\n")
+        for i in range(0,len(line)):
+            temp.append(line[i])
+            if len(temp) == 14: # 4
+                if len(list(set(temp))) == 14: #4
+                    print(i+1)
+                    break
+                else:
+                    temp.pop(0)
+
